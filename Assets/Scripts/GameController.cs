fac4f24f08a65d92493b4af2241b9ce6945d8b3a
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2020-08-25 00:55:12 +0600

    Added the whole project.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject moleContainer;
+
+    public float spawnDuration = 1.5f;
+    public float spawnDecrement = 0.5f;
+    public float minimumSpawnDuration = 0.5f;
+
+    private float resetTimer = 3f;
+
+    public float gameTimer = 15f;
+    public float gameTimerDecrement = 1f;
+
+    public Player player;
+
+    public TextMesh infoText;
+
+    private Mole[] moles;
+
+    public float spawnTimer = 0f;
+    void Start()
+    {
+        moles = moleContainer.GetComponentsInChildren<Mole>();
+       /// moles[Random.Range(0,moles.Length )].Rise();       for understanding the script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
+        {
+          
+
+            gameTimer-= gameTimerDecrement;
+            if (gameTimer > 0f)
+            {
+                moles[Random.Range(0, moles.Length)].Rise();
+
+                spawnDuration -= spawnDecrement;
+                if (spawnDuration < minimumSpawnDuration)
+                {
+                    spawnDuration = minimumSpawnDuration;
+                }
+
+                spawnTimer = spawnDuration;
+                infoText.text = "Hit all the moles\n Time: " + Mathf.Floor(gameTimer)+"\n Your Score: "+player.score;
+            }
+            else
+            {
+                infoText.text = "Game Over!\n Your Score: " + Mathf.Floor(player.score);
+
+                resetTimer -= Time.deltaTime;
+                if (resetTimer <= 0f)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleVR;
+
+public class Player : MonoBehaviour
+{
+
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,transform.forward, out hit))
+        {
+            if (hit.transform.GetComponent<Mole>() != null)
+            {
+                Mole mole = hit.transform.GetComponent<Mole>();
+                mole.OnHit();
+                score++;
+            }
+        }
+    }
+}
